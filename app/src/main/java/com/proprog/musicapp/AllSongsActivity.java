60b51cb1ef3371e0fd339c450a3c711ce4de5011
Muhammad Ahmed <muhammadahmed1339@gmail.com>
2017-09-28 13:24:46 +0200

    try Payment Method

--- app/src/main/java/com/proprog/musicapp/AllSongsActivity.java
+++ app/src/main/java/com/proprog/musicapp/AllSongsActivity.java
@@ -1,13 +1,19 @@
 package com.proprog.musicapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class AllSongsActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_all_songs);
+
     }
+
 }
